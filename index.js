0d2ece80c9343af43140a2209530a33ff5c528a7
arthurgbatista22-hub <arthurgbatista22@gmail.com>
2026-04-23 18:57:25 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ console.log("API KEY:", process.env.GEMINI_API_KEY);
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-flash"
+  model: "gemini-1.5-flash-latest"
 });
 
 async function perguntarIA(msg) {
